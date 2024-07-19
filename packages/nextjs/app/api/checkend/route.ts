8f1845dc39c227d45aa34573a0ab424c8f0bd8de
ATom4869 <arsontomanta@gmail.com>
2024-07-20 04:09:14 +0700

    remove-cluttering-files

--- packages/nextjs/app/api/checkend/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  console.log("hit route checkdb", new Date().getSeconds());
-  return new NextResponse("route endpoint");
-}
